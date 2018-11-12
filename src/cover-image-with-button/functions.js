db60adbead9ec4ae952c3a3c93324e32922df204
Chris Wilcoxson <chris@slushman.com>
2018-11-12 09:51:56 -0600

    Correct dimRatio function to return null only when overlayOpacity is zero.

--- src/cover-image-with-button/functions.js
+++ src/cover-image-with-button/functions.js
@@ -1,5 +1,5 @@
 export const dimRatioToClass = ( ratio ) => {
-	return ( 0 === ratio || 50 === ratio ) ?
+	return ( 0 === ratio ) ?
 		null :
 		'has-background-dim-' + ( 10 * Math.round( ratio / 10 ) );
 };
